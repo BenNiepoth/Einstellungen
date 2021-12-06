68b96d421f1207098391316385b6d383b491c284
Benny Niepoth <benny.niepoth97@gmail.com>
2021-12-06 11:48:46 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'Einstellungen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Einstellungen',
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+      ),
+      home: Einstellungen(),
+    );
+  }
+}
+
+
